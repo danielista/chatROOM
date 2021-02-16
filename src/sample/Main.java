bbcb7753a9e61279e2f425065700fb9ad7a303e3
danielista <61905525+danielista@users.noreply.github.com>
2021-02-16 11:44:25 +0100

    added new message window

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -24,10 +25,12 @@ import sk.kosickaakademia.martinek.chat.database.tajnosti;
 import sk.kosickaakademia.martinek.chat.entity.User;
 import sk.kosickaakademia.martinek.chat.out.Output;
 
+import static javafx.scene.layout.GridPane.*;
+
 
 public class Main extends Application {
     Stage window;
-    Scene chatRoomWindow, loginWindow;
+    Scene chatRoomWindow, loginWindow, newMessageScene;
 
 
     @Override
@@ -39,9 +42,10 @@ public class Main extends Application {
         Text password = new Text("Password");
 
         TextField loginInputText = new TextField();
-        loginInputText.setPromptText("login name");
+        loginInputText.setPromptText("Type your login name here");
+        loginInputText.setPrefWidth(200);
         PasswordField passwordInputText = new PasswordField();
-        passwordInputText.setPromptText("Your password");
+        passwordInputText.setPromptText("Type your password here");
 
         Label errorLabel = new Label();
         errorLabel.setText("Nič tu nefunguje!!!");
@@ -95,18 +99,27 @@ public class Main extends Application {
         gridPane.add(loginInputText, 1, 0);
         gridPane.add(password, 0, 1);
         gridPane.add(passwordInputText, 1, 1);
-        gridPane.add(button1, 0, 3);
+        gridPane.add(button1, 1, 3);
         gridPane.add(button2, 1, 3);
+        GridPane.setHalignment(button2,HPos.RIGHT);
+
         gridPane.add(errorLabel,0,2);
 
-        //Creating a scene object
-        loginWindow = new Scene(gridPane);
 
 
 
         //elementy na 2. scénu:
         Label loginUser = new Label("logged as: ");
         Label loginUserName = new Label("DANKO");  //dorob prepojenie
+
+        Button newMessageButton = new Button("NEW message");
+        newMessageButton.setOnAction(event -> {
+            window.setTitle("NEW MESSAGE");
+            window.setScene(newMessageScene);
+        });
+        Button refreshButton = new Button("REFRESH");
+        Button logoutButton = new Button("LOGOUT");
+
         TextArea messagesArea = new TextArea();
         messagesArea.setMaxWidth(400);
         messagesArea.setMaxHeight(300);
@@ -114,19 +127,53 @@ public class Main extends Application {
         messagesArea.setEditable(false);
 
         // 2. scéna:
+
+        //Instantiating the HBox class
+        HBox hbox = new HBox();
+
+        //Setting the space between the nodes of a HBox pane
+        hbox.setSpacing(10);
+        hbox.getChildren().addAll(loginUser,loginUserName);
+        //Setting the margin to the nodes
+        HBox.setMargin(loginUser, new Insets(10, 0, 0, 20));
+        HBox.setMargin(loginUserName, new Insets(10, 0, 0, 2));
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(newMessageButton,refreshButton,logoutButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+
         BorderPane chatroomPane = new BorderPane();
         chatroomPane.setMinSize(500,500);
 
-        BorderPane.setAlignment(loginUserName,Pos.TOP_CENTER);
-        BorderPane.setMargin(loginUserName,new Insets(10,10,10,10));
-        BorderPane.setAlignment(loginUser,Pos.TOP_LEFT);
-        chatroomPane.setTop(loginUser);
-        chatroomPane.setTop(loginUserName);
+        chatroomPane.setTop(hbox);
         chatroomPane.setCenter(messagesArea);
+        chatroomPane.setRight(vbox);
+        chatroomPane.getRight().setRotate(20);
+        chatroomPane.getRight().setTranslateX(-30);
+
+        //NEW MESSAGE WINDOW ELEMENTS
+        Label komu = new Label("TO: ");
+        Label textSpravy = new Label("Text správy: ");
+        TextField komuTextField = new TextField();
+        TextField teloSpravy = new TextField();
+
+        //NEW MESSAGE GRID PANE
+        GridPane newMessagePane = new GridPane();
+        newMessagePane.setAlignment(Pos.CENTER);
+
+        //Arranging all the nodes in the grid
+        newMessagePane.add(komu, 0, 0);
+        newMessagePane.add(textSpravy, 0, 1);
+        newMessagePane.add(komuTextField, 1, 0);
+        newMessagePane.add(teloSpravy, 1, 1);
 
 
+        //Creating a scene object
+        loginWindow = new Scene(gridPane);
+        chatRoomWindow = new Scene(chatroomPane,600,500);
 
-        chatRoomWindow = new Scene(chatroomPane,600,600);
+        newMessageScene = new Scene(newMessagePane);
 
 
 
